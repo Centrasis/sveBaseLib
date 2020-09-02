9f4ecf327ba6ab605134107916ec645eba44b9be
Felix Lehner <f.lehner@tu-bs.de>
2020-09-02 17:41:46 +0200

    dowgraded module and target to es6 and es5 due to webpack issue

--- dist/SVEProject.js
+++ dist/SVEProject.js
@@ -1,4 +1,3 @@
-"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -8,60 +7,94 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SVEProject = exports.isProjectInitializer = exports.SVEProjectType = void 0;
-const SVEAccount_1 = require("./SVEAccount");
-const SVEGroup_1 = require("./SVEGroup");
-const SVESystemInfo_1 = require("./SVESystemInfo");
-const SVEData_1 = require("./SVEData");
-var SVEProjectType;
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+import { SVEAccount } from './SVEAccount';
+import { SVEGroup } from './SVEGroup';
+import { SVESystemInfo } from './SVESystemInfo';
+import { SVEData } from './SVEData';
+export var SVEProjectType;
 (function (SVEProjectType) {
     SVEProjectType[SVEProjectType["Vacation"] = 0] = "Vacation";
     SVEProjectType[SVEProjectType["Sales"] = 1] = "Sales";
-})(SVEProjectType = exports.SVEProjectType || (exports.SVEProjectType = {}));
-function isProjectInitializer(init) {
+})(SVEProjectType || (SVEProjectType = {}));
+export function isProjectInitializer(init) {
     return typeof init !== "number";
 }
-exports.isProjectInitializer = isProjectInitializer;
-class SVEProject {
-    constructor(idx, handler, onReady) {
+var SVEProject = /** @class */ (function () {
+    function SVEProject(idx, handler, onReady) {
+        var _this = this;
         this.id = NaN;
         this.name = "";
         this.type = SVEProjectType.Vacation;
         // if get by id
         if (!isProjectInitializer(idx)) {
-            if (SVESystemInfo_1.SVESystemInfo.getIsServer()) {
+            if (SVESystemInfo.getIsServer()) {
                 if (onReady !== undefined)
                     onReady(this);
             }
             else {
-                () => __awaiter(this, void 0, void 0, function* () {
-                    const response = yield fetch(SVESystemInfo_1.SVESystemInfo.getInstance().sources.sveService + '/project/' + idx, {
-                        method: 'GET',
-                        headers: {
-                            'Accept': 'application/json',
-                            'Content-Type': 'application/json'
-                        }
-                    });
-                    if (response.status < 400) {
-                        response.json().then((val) => {
-                            this.id = val.id;
-                            this.name = val.name;
-                            this.type = val.type;
-                            this.handler = handler;
-                            this.owner = new SVEAccount_1.SVEAccount({ id: val.owner }, (s) => {
-                                this.group = new SVEGroup_1.SVEGroup(val.group, handler, (self) => {
+                (function () { return __awaiter(_this, void 0, void 0, function () {
+                    var response;
+                    var _this = this;
+                    return __generator(this, function (_a) {
+                        switch (_a.label) {
+                            case 0: return [4 /*yield*/, fetch(SVESystemInfo.getInstance().sources.sveService + '/project/' + idx, {
+                                    method: 'GET',
+                                    headers: {
+                                        'Accept': 'application/json',
+                                        'Content-Type': 'application/json'
+                                    }
+                                })];
+                            case 1:
+                                response = _a.sent();
+                                if (response.status < 400) {
+                                    response.json().then(function (val) {
+                                        _this.id = val.id;
+                                        _this.name = val.name;
+                                        _this.type = val.type;
+                                        _this.handler = handler;
+                                        _this.owner = new SVEAccount({ id: val.owner }, function (s) {
+                                            _this.group = new SVEGroup(val.group, handler, function (self) {
+                                                if (onReady !== undefined)
+                                                    onReady(_this);
+                                            });
+                                        });
+                                    });
+                                }
+                                else {
                                     if (onReady !== undefined)
                                         onReady(this);
-                                });
-                            });
-                        });
-                    }
-                    else {
-                        if (onReady !== undefined)
-                            onReady(this);
-                    }
-                });
+                                }
+                                return [2 /*return*/];
+                        }
+                    });
+                }); });
             }
         }
         else {
@@ -75,67 +108,79 @@ class SVEProject {
                 onReady(this);
         }
     }
-    getID() {
+    SVEProject.prototype.getID = function () {
         return this.id;
-    }
-    getName() {
+    };
+    SVEProject.prototype.getName = function () {
         return this.name;
-    }
-    getType() {
+    };
+    SVEProject.prototype.getType = function () {
         return this.type;
-    }
-    getOwner() {
+    };
+    SVEProject.prototype.getOwner = function () {
+        var _this = this;
         if (typeof this.owner === "number") {
-            return new Promise((resolve, reject) => {
-                this.owner = new SVEAccount_1.SVEAccount({ id: this.owner }, (s) => {
-                    resolve(this.owner);
+            return new Promise(function (resolve, reject) {
+                _this.owner = new SVEAccount({ id: _this.owner }, function (s) {
+                    resolve(_this.owner);
                 });
             });
         }
         else {
-            return new Promise((resolve, reject) => {
-                if (this.owner == undefined) {
+            return new Promise(function (resolve, reject) {
+                if (_this.owner == undefined) {
                     reject();
                 }
                 else {
-                    resolve(this.owner);
+                    resolve(_this.owner);
                 }
             });
         }
-    }
-    getGroup() {
+    };
+    SVEProject.prototype.getGroup = function () {
         return this.group;
-    }
-    getData() {
-        return new Promise((resolve, reject) => {
-            () => __awaiter(this, void 0, void 0, function* () {
-                const response = yield fetch(SVESystemInfo_1.SVESystemInfo.getInstance().sources.sveService + '/project/' + this.id + '/data/list', {
-                    method: "GET"
-                });
-                if (response.status < 400) {
-                    response.json().then(val => {
-                        let r = [];
-                        let i = 0;
-                        if (val.length > 0) {
-                            val.foreach((v) => {
-                                r.push(new SVEData_1.SVEData(this.handler, { id: v.id, parentProject: this, type: v.type }, (s) => {
-                                    i++;
-                                    if (i >= val.length) {
+    };
+    SVEProject.prototype.getData = function () {
+        var _this = this;
+        return new Promise(function (resolve, reject) {
+            (function () { return __awaiter(_this, void 0, void 0, function () {
+                var response;
+                var _this = this;
+                return __generator(this, function (_a) {
+                    switch (_a.label) {
+                        case 0: return [4 /*yield*/, fetch(SVESystemInfo.getInstance().sources.sveService + '/project/' + this.id + '/data/list', {
+                                method: "GET"
+                            })];
+                        case 1:
+                            response = _a.sent();
+                            if (response.status < 400) {
+                                response.json().then(function (val) {
+                                    var r = [];
+                                    var i = 0;
+                                    if (val.length > 0) {
+                                        val.foreach(function (v) {
+                                            r.push(new SVEData(_this.handler, { id: v.id, parentProject: _this, type: v.type }, function (s) {
+                                                i++;
+                                                if (i >= val.length) {
+                                                    resolve(r);
+                                                }
+                                            }));
+                                        });
+                                    }
+                                    else {
                                         resolve(r);
                                     }
-                                }));
-                            });
-                        }
-                        else {
-                            resolve(r);
-                        }
-                    }, err => reject(false));
-                }
-                else {
-                    reject(false);
-                }
-            });
+                                }, function (err) { return reject(false); });
+                            }
+                            else {
+                                reject(false);
+                            }
+                            return [2 /*return*/];
+                    }
+                });
+            }); });
         });
-    }
-}
-exports.SVEProject = SVEProject;
+    };
+    return SVEProject;
+}());
+export { SVEProject };
--- dist/SVESystemInfo.js
+++ dist/SVESystemInfo.js
@@ -1,4 +1,3 @@
-"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -8,11 +7,36 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SVESystemInfo = void 0;
-const SVEAccount_1 = require("./SVEAccount");
-class SVESystemInfo {
-    constructor() {
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+import { SVEAccount } from "./SVEAccount";
+var SVESystemInfo = /** @class */ (function () {
+    function SVESystemInfo() {
         this.systemState = {
             authorizationSystem: false,
             basicSystem: false,
@@ -30,85 +54,105 @@ class SVESystemInfo {
         };
         SVESystemInfo.isServer = false;
     }
-    static getIsServer() {
+    SVESystemInfo.getIsServer = function () {
         return SVESystemInfo.isServer;
-    }
-    static getInstance() {
+    };
+    SVESystemInfo.getInstance = function () {
         if (!SVESystemInfo.instance) {
             SVESystemInfo.instance = new SVESystemInfo();
         }
         return SVESystemInfo.instance;
-    }
-    static initSystem() {
-        return new Promise((resolve, reject) => {
-            this.instance.systemState = {
+    };
+    SVESystemInfo.initSystem = function () {
+        var _this = this;
+        return new Promise(function (resolve, reject) {
+            _this.instance.systemState = {
                 authorizationSystem: false,
                 basicSystem: false,
                 tokenSystem: false
             };
-            if (this.getInstance().sources.sveService !== undefined) {
-                () => __awaiter(this, void 0, void 0, function* () {
-                    const response = yield fetch(this.getInstance().sources.sveService + '/check', {
-                        method: "GET"
+            if (_this.getInstance().sources.sveService !== undefined) {
+                (function () { return __awaiter(_this, void 0, void 0, function () {
+                    var response;
+                    var _this = this;
+                    return __generator(this, function (_a) {
+                        switch (_a.label) {
+                            case 0: return [4 /*yield*/, fetch(this.getInstance().sources.sveService + '/check', {
+                                    method: "GET"
+                                })];
+                            case 1:
+                                response = _a.sent();
+                                if (response.status < 400) {
+                                    response.json().then(function (val) {
+                                        _this.instance.systemState = val.status;
+                                        resolve(true);
+                                    }, function (err) { return reject(false); });
+                                }
+                                else {
+                                    reject(false);
+                                }
+                                return [2 /*return*/];
+                        }
                     });
-                    if (response.status < 400) {
-                        response.json().then(val => {
-                            this.instance.systemState = val.status;
-                            resolve(true);
-                        }, err => reject(false));
-                    }
-                    else {
-                        reject(false);
-                    }
-                });
+                }); });
             }
             else {
                 reject(false);
             }
         });
-    }
-    static getSystemStatus() {
+    };
+    SVESystemInfo.getSystemStatus = function () {
         return this.getInstance().systemState;
-    }
-    static getFullSystemState() {
-        return new Promise((resolve, reject) => {
-            () => __awaiter(this, void 0, void 0, function* () {
-                const response = yield fetch(SVESystemInfo.getAPIRoot() + "/check", {
-                    method: 'GET',
-                    headers: {
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json'
+    };
+    SVESystemInfo.getFullSystemState = function () {
+        var _this = this;
+        return new Promise(function (resolve, reject) {
+            (function () { return __awaiter(_this, void 0, void 0, function () {
+                var response;
+                return __generator(this, function (_a) {
+                    switch (_a.label) {
+                        case 0: return [4 /*yield*/, fetch(SVESystemInfo.getAPIRoot() + "/check", {
+                                method: 'GET',
+                                headers: {
+                                    'Accept': 'application/json',
+                                    'Content-Type': 'application/json'
+                                }
+                            })];
+                        case 1:
+                            response = _a.sent();
+                            if (response.status < 400) {
+                                response.json().then(function (val) {
+                                    if (!("loggedInAs" in val)) {
+                                        resolve({
+                                            authorizationSystem: val.status.authorizationSystem,
+                                            basicSystem: val.status.basicSystem,
+                                            tokenSystem: val.status.tokenSystem
+                                        });
+                                    }
+                                    else {
+                                        var loggedInAs_1 = new SVEAccount(val.loggedInAs, function (s) {
+                                            resolve({
+                                                authorizationSystem: val.status.authorizationSystem,
+                                                basicSystem: val.status.basicSystem,
+                                                tokenSystem: val.status.tokenSystem,
+                                                user: loggedInAs_1
+                                            });
+                                        });
+                                    }
+                                }, function (err) { return reject({}); });
+                            }
+                            else {
+                                reject({});
+                            }
+                            return [2 /*return*/];
                     }
                 });
-                if (response.status < 400) {
-                    response.json().then(val => {
-                        if (!("loggedInAs" in val)) {
-                            resolve({
-                                authorizationSystem: val.status.authorizationSystem,
-                                basicSystem: val.status.basicSystem,
-                                tokenSystem: val.status.tokenSystem
-                            });
-                        }
-                        else {
-                            let loggedInAs = new SVEAccount_1.SVEAccount(val.loggedInAs, (s) => {
-                                resolve({
-                                    authorizationSystem: val.status.authorizationSystem,
-                                    basicSystem: val.status.basicSystem,
-                                    tokenSystem: val.status.tokenSystem,
-                                    user: loggedInAs
-                                });
-                            });
-                        }
-                    }, err => reject({}));
-                }
-                else {
-                    reject({});
-                }
-            });
+            }); });
         });
-    }
-    static getAPIRoot() {
+    };
+    SVESystemInfo.getAPIRoot = function () {
         return (SVESystemInfo.getInstance().sources.sveService !== undefined) ? SVESystemInfo.getInstance().sources.sveService : "";
-    }
-}
-exports.SVESystemInfo = SVESystemInfo;
+    };
+    return SVESystemInfo;
+}());
+export { SVESystemInfo };
